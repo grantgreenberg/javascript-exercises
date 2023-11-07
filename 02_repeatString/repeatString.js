fd5bc8626569d58913c5aea12f08e2eb5391f167
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-07 10:40:27 -0700

    feat: updated repeatString.js algorithm and passed all tests in spec.js.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,25 @@
-const repeatString = function() {
+const repeatString = function(textToRepeat, numberOfRepeats) {
 
+    i = 0;
+    keepGoing = true;
+    let returnString = "";
+
+    while (keepGoing) {
+
+        if (numberOfRepeats < 0) {
+            return "ERROR";
+        }
+        else if (i < numberOfRepeats) {
+            returnString += textToRepeat;
+        }
+        else {
+            keepGoing = false;
+            return returnString;
+        }
+
+        i++;
+
+    }
 };
 
 // Do not edit below this line
