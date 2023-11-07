2f19103c4d73faee5b8758ddad3f3b16ca705324
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-07 11:54:18 -0700

    feat: updated reverseString.js to output reversed string and passed all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
-};
+const reverseString = function(text) {
+    let characterArray = [];
+    let textLength = text.length;
+    for (let i=0; i<textLength; i++) {
+        let char = text.charAt(0);
+        characterArray.unshift(char);
+        text = text.slice(1);
+    }
+    return characterArray.join("")
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
