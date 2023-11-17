d21ec7359886e977fe74dbdf98b5c9c17db1ef8b
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-17 12:52:25 -0700

    feat: update leapYears function to return valid leap years.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
 const leapYears = function() {
-
+    if (arguments[0] % 100 == 0 && arguments[0] % 400 == 0) {
+        return true;
+    }
+    else if (arguments[0] % 4 == 0 && arguments[0] % 100 != 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
