e5347607184e7f68c24cda9b30311bebc3797f5d
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-17 11:38:50 -0700

    feat: updated the removeFromArray function to remove array elements.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let len = arguments.length;
+    let arr = arguments[0];
 
+    for (let i=1;i<len;i++) {
+        if (arr.indexOf(arguments[i]) >= 0) {
+            let pos = arr.indexOf(arguments[i]);
+            arr.splice(pos, 1);
+        } else { continue; }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
