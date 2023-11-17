1cf1f6e46622f0363f42734d517c30526345c1cd
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-17 12:30:11 -0700

    feat: update sumAll function to add given numerical values.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
 const sumAll = function() {
-
-};
+    let sum = 0;
+    if(arguments[0] >= 0 && typeof arguments[0] === "number" 
+    && arguments[1] >= 0 && typeof arguments[1] === "number" ) {
+        if (arguments[0] < arguments[1]) {
+            for (let i=arguments[0];i<=arguments[1];i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        else {
+            for (let i=arguments[1];i<=arguments[0];i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
