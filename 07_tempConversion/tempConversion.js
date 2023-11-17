453b92255cc9a1b188b0e0cd10572b7cafabf794
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-17 13:03:06 -0700

    feat: update temp conversion functions for celsius and fahrenheit.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
 const convertToCelsius = function() {
+  let degF = (arguments[0]-32)*(5/9);
+  return Math.round(degF * 10) / 10;
 };
 
 const convertToFahrenheit = function() {
+  let degC = (arguments[0]*(9/5))+32;
+  return Math.round(degC * 10) / 10;
 };
 
 // Do not edit below this line
