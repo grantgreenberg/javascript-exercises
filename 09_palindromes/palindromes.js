88b487e3b35a062cb4aed6c380021b2ddc3114eb
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-25 13:39:55 -0700

    feat: created palindromes function that returns whether or not a string is a palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (text) {
 
+    let arr = text.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase().split('');
+    let arrCopy = arr.map((x) => x)
+    let reversedArr = arrCopy.reverse();
+
+    return arr.join('') === reversedArr.join('');
 };
 
 // Do not edit below this line
