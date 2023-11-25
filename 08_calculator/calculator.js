8af46cb5d5d151e133f3e0ddaafc9c8ba847b099
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-25 13:00:22 -0700

    feat: create working calculator functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return parseInt(number1) + parseInt(number2);
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return parseInt(number1) - parseInt(number2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	array.forEach(element => {
+    total += parseInt(element);
+  });
+  return total;
 };
 
-const multiply = function() {
 
+const multiply = function(array) {
+  let total = 1;
+	array.forEach(element => {
+    total *= parseInt(element);
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  total = 1;
+  if (number == 0) {
+    return 1;
+  }
+  else {
+    for (let i=1;i<=number;i++) {
+      total *= i;
+    }
+  };
+	return total;
 };
 
 // Do not edit below this line
