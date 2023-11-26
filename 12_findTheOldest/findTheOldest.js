4179778232e31978ec4edb3d647a35cee263b610
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-26 15:33:42 -0700

    feat: created findTheOldest function to return oldest person from array of person objects.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b) {
+        let lastGuy, nextGuy;
 
+        if (a.yearOfDeath != null) {
+            lastGuy = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            lastGuy = new Date().getFullYear() - a.yearOfBirth;
+        }
+
+        if (b.yearOfDeath != null) {
+            nextGuy = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            nextGuy = new Date().getFullYear() - b.yearOfBirth;
+        }
+
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
