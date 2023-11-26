a4f6958399c6fb850631fda237e8d72670891aa3
Grant Greenberg <19313835+grantgreenberg@users.noreply.github.com>
2023-11-25 22:20:55 -0700

    feat: created fibonacci function to return the fibonacci value at the given array index value.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) {
+        return 'OOPS';
+    }
+    else {
+        return fib(input);
+    }
+};
 
+function fib(num) {
+    let fibArray = [];
+    let n1 = 0, n2 = 1, nextTerm;
+    nextTerm = n1 + n2;
+    fibArray.push(nextTerm);
+
+
+    for (let i = 1; i <= parseInt(num); i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+        fibArray.push(nextTerm);
+    }
+
+    return parseInt(fibArray.splice(num-1, 1).join(''));
 };
 
 // Do not edit below this line
